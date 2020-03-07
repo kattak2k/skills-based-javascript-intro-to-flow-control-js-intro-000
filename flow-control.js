27cb5714a1ea61498a1550cacdbfe9df0742c624
Ravi <kkiran.ror@gmail.com>
2020-03-07 21:51:45 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -13,12 +13,12 @@ function teenager(age) {
 };
 
 function ageChecker(age) {
-  if (age >= 13 && age <= 19) {
-    console.log("You are a teenager!");
+  if (age >= 13 && age <= 19 {
+    return  "You are a teenager!";
   } else if (age < 12){
-    console.log("You are a kid");
+    return  "You are a kid";
   } else{
-    console.log("You are a grownup");
+    return  "You are a grownup";
   };
 };
 
