3333eb863ebf590cd23645c6c802d530027b1f73
Ravi <kkiran.ror@gmail.com>
2020-03-07 21:50:16 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -3,3 +3,33 @@ function basicTeenager(age) {
     console.log("You are a teenager!");
   }
 }
+
+function teenager(age) {
+  if (age >= 13 && age <= 19) {
+    console.log("You are a teenager!");
+  } else {
+    console.log("You are not a teenager");
+  };
+};
+
+function ageChecker(age) {
+  if (age >= 13 && age <= 19) {
+    console.log("You are a teenager!");
+  } else if (age < 12){
+    console.log("You are a kid");
+  } else{
+    console.log("You are a grownup");
+  };
+};
+
+function ternaryTeenager(age) {
+
+};
+
+function switchAge(age) {
+  if (age >= 13 && age <= 19) {
+    console.log("You are a teenager!");
+  } else {
+    console.log("You have an age");
+  };
+};
