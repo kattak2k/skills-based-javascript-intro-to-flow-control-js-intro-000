9f7cf775857f7ff89da56361e2183dea082d196a
Ravi <kkiran.ror@gmail.com>
2020-03-07 22:54:32 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -32,5 +32,5 @@ function switchAge(age) {
     case age < 13: return "You have an age";
     case age <= 19:  return "You are a teenager";
     case age < 100: return "You have an age";
-    default: return "not an age"
+    default: return "not an age";
 }
