958658ad1cf765e60830ac686a8cb37916b7d82b
Ravi <kkiran.ror@gmail.com>
2020-03-07 21:55:33 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -4,3 +4,33 @@ function basicTeenager(age) {
      return "You are a teenager!";
   }
 }
+
+function teenager(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!" ;
+  } else {
+    return "You are not a teenager" ;
+  }
+}
+
+function ageChecker(age) {
+  if (age >= 13 && age <= 19 {
+    return "You are a teenager!";
+  } else if (age < 12){
+    return "You are a kid";
+  } else {
+    return "You are a grownup";
+  };
+}
+
+function ternaryTeenager(age) {
+
+}
+
+function switchAge(age) {
+  if (age >= 13 && age <= 19) {
+    console.log("You are a teenager!");
+  } else {
+    console.log("You have an age");
+  };
+}
